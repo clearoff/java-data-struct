41342be227580053b0ecea08407df0a3507bf16f
liaozihao <liaozihao@meituan.com>
2019-01-24 12:00:45 +0800

    理解跳跃表数据结构

--- netty-helloworld/src/main/java/com/datastruct/SkipList.java
+++ netty-helloworld/src/main/java/com/datastruct/SkipList.java
@@ -6,9 +6,15 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * 1.每次插入数据的时候随机产生的level:决定了新节点的层数；
+ * 2.数组update的作用：用以存储新节点所有层数上，各自的前一个节点的信息；
+ * 3.节点内的forwards数组：用以存储该节点所有层的下一个节点的信息；
+ * 4.当所有节点的最大层级变量maxlevel=1的时候，跳表退化成一个普通链表
+ */
 public class SkipList {
 
-    private static final Integer MAX_LEVEL = 3;
+    private static final Integer MAX_LEVEL = 2;
 
     private int levelCount = 1;
 
@@ -28,21 +34,26 @@ public class SkipList {
     }
 
     public void insert(int value) {
-        int level = randomLevel();
+        int level = randomLevel();  //产生一个随机的level决定当前node的最大层层数
         Node newNode = new Node();
         newNode.data = value;
         newNode.maxLevel = level;
         Node update[] = new Node[level];
+
+        // 创建从lowLevel到当前节点的maxLeve的所有节点
         for (int i = 0; i < level; ++i) {
             update[i] = head;
         }
 
         // record every level largest value which smaller than insert value in update[]
+        // 插入上面创建的节点
         Node p = head;
         for (int i = level - 1; i >= 0; --i) {
+            // 按照Level查找插入
             while (p.forwards[i] != null && p.forwards[i].data < value) {
                 p = p.forwards[i];
             }
+            // 保存搜索路径上的最后一个节点
             update[i] = p;// use update save node in search path
         }
 
@@ -52,7 +63,7 @@ public class SkipList {
             update[i].forwards[i] = newNode;
         }
 
-        // update node hight
+        // 更新当前最大level
         if (levelCount < level) levelCount = level;
     }
 
@@ -108,13 +119,7 @@ public class SkipList {
         for (int i = 0; i < 10; i++) {
             skipList.insert(i);
         }
+        skipList.insert(11);
         skipList.printAll();
-
-        Map<String, Integer> map = new LinkedHashMap<>();
-        map.put("hehe", 1);
-        map.put("hehe2", 2);
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue());
-        }
     }
 }
